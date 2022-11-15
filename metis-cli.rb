f618f721c4d273eb1c0020ef0c9f5466aae6a3ab
davidshnur <david.shnur@allcloud.io>
2022-11-15 17:26:04 +0200

    modify url and sha in metis cli

--- metis-cli.rb
+++ metis-cli.rb
@@ -1,15 +1,15 @@
 class MetisCli < Formula
-  desc 
-  homepage 
-  url "https://github.com/metis-data/metis-cli/releases/download/0.4.28/metis_macos_0.4.28"
+  desc ""
+  homepage ""
+  url "https://github.com/metis-data/metis-cli/releases/download/0.4.27/metis_macos_0.4.27"
   version "1.0.0"
-  sha256 
+  sha256 "ae15e5bde8ea0405fa1dda500e698b4a0fb47dfc1233a2bcc12e905207cfa1dd"
   
   
   def install
     require "fileutils"
 
-    File.rename("metis_macos_0.4.28", "metis-cli")
+    File.rename("metis_macos_0.4.27", "metis-cli")
     FileUtils.chmod("+x", "metis-cli")
     bin.install "metis-cli"
   end
