4863677d9a9c0a65df453aee35cb86b61e61c1dc
davidshnur <david.shnur@allcloud.io>
2022-11-15 08:47:17 +0200

    update metis cli file

--- metis-cli.rb
+++ metis-cli.rb
@@ -1,17 +1,17 @@
 class MetisCli < Formula
-    desc ""
-    homepage ""
-    url "https://github.com/metis-data/metis-cli/releases/download/0.4.5/metis_macos_0.4.5"
-    version "1.0.0"
-    sha256 "c72c299b2013171d2ad76a5e22704313ce17ef8d26363b80b6bcb40dce145ca4"
-    
-    
-    def install
-      require "fileutils"
+  desc ""
+  homepage ""
+  url "https://github.com/metis-data/metis-cli/releases/download/0.4.5/metis_macos_0.4.5"
+  version "1.0.0"
+  sha256 "c72c299b2013171d2ad76a5e22704313ce17ef8d26363b80b6bcb40dce145ca4"
   
-      File.rename("metis_macos_0.4.5", "metis-cli")
-      FileUtils.chmod("+x", "metis-cli")
-      bin.install "metis-cli"
-    end
   
-  end  
+  def install
+    require "fileutils"
+
+    File.rename("metis_macos_0.4.5", "metis-cli")
+    FileUtils.chmod("+x", "metis-cli")
+    bin.install "metis-cli"
+  end
+
+end
